57796f7c89866a4edb04dc719f013f593d55b366
Sethu Gopalan <Sethu.Gopalan@cytel.com>
2022-10-25 14:14:06 -0400

    final update before put into pipe

--- app_folder/app.py
+++ app_folder/app.py
@@ -1,9 +1,17 @@
 import dash
 import dash_labs as dl
 import dash_bootstrap_components as dbc
+import dash_auth
+from users_access import USERNAME_PASSWORD_PAIRS
+
 
 app = dash.Dash(__name__, plugins=[dl.plugins.pages],
                 external_stylesheets=[dbc.themes.SLATE])
+auth = dash_auth.BasicAuth(
+    app,
+    USERNAME_PASSWORD_PAIRS
+)
+
 
 navbar = dbc.NavbarSimple(
     dbc.DropdownMenu(
